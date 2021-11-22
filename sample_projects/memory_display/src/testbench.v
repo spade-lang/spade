7e3e8b2a891d6188d62cea9d873ec85884ee918b
TheZoq2 <frans.skarman@protonmail.com>
2021-11-22 22:04:35 +0100

    More progress on memory display

--- sample_projects/memory_display/src/testbench.v
+++ sample_projects/memory_display/src/testbench.v
@@ -17,8 +17,9 @@ module spi_tx_tb();
 
 
     reg rst;
-    wire sclk;
+    wire cs;
     wire mosi;
+    wire sclk;
     wire busy;
     reg[8:0] to_transmit;
 
@@ -26,42 +27,17 @@ module spi_tx_tb();
 
     initial begin
         rst = 1;
-        to_transmit <= 9'b100000000;
         #4
         rst = 0;
 
-        repeat (2000) @(negedge clk);
-        `ASSERT_EQ(busy, 0);
-        `ASSERT_EQ(sclk, 1);
-
-        to_transmit <= 9'b0_1011_0010;
-        @(negedge clk)
-        @(negedge clk)
-
-        for (i = 0; i < 8; i = i + 1) begin
-            // Clock should still be high and data set
-            `ASSERT_EQ(sclk, 1);
-            `ASSERT_EQ(mosi, to_transmit[i]);
-            repeat(500) @(negedge clk);
-            // Clock should have fallen
-            `ASSERT_EQ(sclk, 0);
-            repeat(500) @(negedge clk);
-        end
-
-        `ASSERT_EQ(sclk, 1);
-
-
-        `ASSERT_EQ(busy, 0);
-
-        #100
+        #400000
 
         `END_TEST
     end
 
-    spi_tx uut
+    main uut
         ( ._i_clk(clk)
         , ._i_rst(rst)
-        , ._i_to_transmit(to_transmit)
-        , .__output({sclk, mosi, busy})
+        , .__output({cs, mosi, sclk})
         );
 endmodule
--- sample_projects/memory_display/top.v
+++ sample_projects/memory_display/top.v
@@ -3,20 +3,22 @@ module top (
     input clk,
 
     output[7:0] pmod7,
-    input[3:0] pmod0,
+    output[3:0] pmod0,
     output[3:0] pmod0Lower
 );
-    wire rst;
-    assign rst = pmod0[2];
+    // assign rst = pmod0[2];
+
+    reg rst = 1;
+    always @(posedge clk) begin
+        rst <= 0;
+    end
 
     assign pmod0Lower = pmod0;
 
     main main
         ( ._i_clk(clk)
         , ._i_rst(rst)
-        , ._i_sclk_unsync(pmod0[0])
-        , ._i_mosi_unsync(pmod0[1])
-        , .__output(pmod7)
+        , .__output({pmod0[2], pmod0[1], pmod0[0]})
         );
 endmodule
 
