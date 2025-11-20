eb9e32a5dba8637a861ea886003beb10fefbdff4
TheZoq2 <frans.skarman@protonmail.com>
2025-11-20 13:03:04 +0100

    Use native cargo publish

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/env sh
 
-cargo release --exclude vcd-translate --exclude spade-python --exclude spade-simulation-ext --exclude spade-cxx --exclude spade-wordlength-inference --exclude spade-tests --no-tag --no-push $@
+cargo publish --workspace --exclude vcd-translate --exclude spade-python --exclude spade-simulation-ext --exclude spade-cxx --exclude spade-wordlength-inference --exclude spade-tests --exclude spade-language-server --exclude spade-surfer-plugin --exclude spadedoc
