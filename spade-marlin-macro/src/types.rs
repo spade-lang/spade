393ff03e4623376c1e2f9ef040bd035e11902f0f
TheZoq2 <mail@fransskarman.com>
2026-02-23 20:46:31 +0100

    Add support for int and generate PhantomData

--- spade-marlin-macro/src/types.rs
+++ spade-marlin-macro/src/types.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use itertools::Itertools;
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 use spade::compiler_state::CompilerState;
@@ -170,7 +171,12 @@ impl TypeDeclarationExt for TypeDeclaration {
         let raw_generics = self
             .generic_args
             .iter()
-            .map(|param| param.name_id().expect("Found a hidden generic").mirror_local())
+            .map(|param| {
+                param
+                    .name_id()
+                    .expect("Found a hidden generic")
+                    .mirror_local()
+            })
             .collect::<Vec<_>>();
 
         let impl_generics = match self.generic_args.as_slice() {
@@ -302,19 +308,49 @@ impl TypeDeclarationExt for TypeDeclaration {
             spade_hir::TypeDeclKind::Struct(s) => {
                 let name = &self.name.mirror_local();
 
-                let fields = s.members.0.iter().map(
-                    |Parameter {
-                         no_mangle: _,
-                         name,
-                         ty,
-                         field_translator: _,
-                     }| {
-                        let name = format_ident!("{}", &name.as_str());
-                        let ty = ty.mirror(primitive_map);
+                // FIXME: Figure out which parameters need phantom data instead of blanket
+                // phantom data'ing them
+                let phantom_args = self
+                    .generic_args
+                    .iter()
+                    .filter_map(|arg| {
+                        match arg.meta {
+                            spade_types::meta_types::MetaType::Type => {
+                                Some(arg.name
+                                    .name_id()
+                                    .expect("Found hidden generic in type")
+                                    .mirror_local())
+                            },
+                            spade_types::meta_types::MetaType::Any |
+                            spade_types::meta_types::MetaType::Number |
+                            spade_types::meta_types::MetaType::Int |
+                            spade_types::meta_types::MetaType::Uint |
+                            spade_types::meta_types::MetaType::Bool |
+                            spade_types::meta_types::MetaType::Str => {
+                                None
+                            }
+                        }
+                    })
+                    .collect::<Vec<_>>();
 
-                        quote! {#name : #ty}
-                    },
-                );
+                let fields = s
+                    .members
+                    .0
+                    .iter()
+                    .map(
+                        |Parameter {
+                             no_mangle: _,
+                             name,
+                             ty,
+                             field_translator: _,
+                         }| {
+                            let name = format_ident!("{}", &name.as_str());
+                            let ty = ty.mirror(primitive_map);
+
+                            quote! {#name : #ty}
+                        },
+                    )
+                    .chain([quote!(phantom: std::marker::PhantomData<(#(#phantom_args),*)>)]);
 
                 let sizes = s.members.0.iter().map(|Parameter { ty, .. }| {
                     let ty = ty.mirror_with_turbofish(primitive_map);
@@ -374,7 +410,10 @@ trait TypeParamExt {
 }
 impl TypeParamExt for TypeParam {
     fn mirror(&self, with_traits: Option<TokenStream>) -> TokenStream {
-        let name = self.name_id().expect("Attempted to mirror a type with no generics").mirror_local();
+        let name = self
+            .name_id()
+            .expect("Attempted to mirror a type with no generics")
+            .mirror_local();
         match self.meta {
             spade_types::meta_types::MetaType::Type => {
                 let with_traits = with_traits.map(|t| quote!(: #t));
@@ -432,6 +471,10 @@ pub fn primitive_map(compiler_state: &CompilerState) -> PrimitiveMap {
             ["uint"].as_slice(),
             quote! {spade_marlin::type_translation::SpadeUint},
         ),
+        (
+            ["int"].as_slice(),
+            quote! {spade_marlin::type_translation::SpadeInt},
+        ),
         (["bool"].as_slice(), quote! {bool}),
         (
             ["std", "option", "Option"].as_slice(),
@@ -470,17 +513,17 @@ pub fn mirror_types(compiler_state: &CompilerState) -> TokenStream {
         for segment in &name.1 .0[0..(name.1 .0.len() - 1)] {
             let ModEntry::Submod(_, next) = module
                 .entry(segment.unwrap_named().as_str())
-                .or_insert(ModEntry::Submod(segment.unwrap_named().as_str(), HashMap::new()))
+                .or_insert(ModEntry::Submod(
+                    segment.unwrap_named().as_str(),
+                    HashMap::new(),
+                ))
             else {
                 panic!("Found {segment} in {name} to be both a type and a module")
             };
             module = next
         }
 
-        module.insert(
-            &name.1.tail().unwrap_named().as_str(),
-            ModEntry::Def(def),
-        );
+        module.insert(&name.1.tail().unwrap_named().as_str(), ModEntry::Def(def));
     }
 
     let result = ModEntry::Submod("spade_types", modules).emit();
