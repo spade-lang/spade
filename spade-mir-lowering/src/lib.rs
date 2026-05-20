718db9ba5bfd077f22339cb2a38bcb3f04717300
TheZoq2 <mail@fransskarman.com>
2026-05-20 11:17:18 +0200

    More MIR lowering

--- spade-mir-lowering/src/lib.rs
+++ spade-mir-lowering/src/lib.rs
@@ -1,9 +1,22 @@
 mod types;
 
+use spade_common::location_info::Loc;
+use spade_diagnostics::Diagnostic;
 use spade_lir::{self as lir, LirArg};
 use spade_mir::{self as mir, MirInput};
 
 use crate::types::TypeExt;
+
+pub type Result<T> = std::result::Result<T, Diagnostic>;
+
+trait ValueNameExt {
+    fn lower_fwd(&self) -> lir::ValueName;
+}
+impl ValueNameExt for mir::ValueName {
+    fn lower_fwd(&self) -> lir::ValueName {
+        lir::ValueName::Forward(self.clone())
+    }
+}
 
 pub(crate) trait EntityExt {
     fn lower(&self) -> lir::Entity;
@@ -25,24 +38,19 @@ impl EntityExt for mir::Entity {
         let (output_back, output_fwd) = output_type.lower();
 
         let output_args = [
-            output_back.map(|ty| {
-                LirArg {
-                    name: "__output".to_string(),
-                    val_name: lir::ValueName::OutputBack,
-                    ty,
-                    no_mangle: None,
-                }
+            output_back.map(|ty| LirArg {
+                name: "__output".to_string(),
+                val_name: lir::ValueName::OutputBack,
+                ty,
+                no_mangle: None,
             }),
-            output_fwd.map(|ty| {
-                LirArg {
-                    name: "__input".to_string(),
-                    val_name: lir::ValueName::OutputFwd,
-                    ty,
-                    no_mangle: None,
-                }
-            })
+            output_fwd.map(|ty| LirArg {
+                name: "__input".to_string(),
+                val_name: lir::ValueName::OutputFwd,
+                ty,
+                no_mangle: None,
+            }),
         ];
-
 
         let arguments: Vec<_> = inputs
             .iter()
@@ -85,18 +93,25 @@ impl EntityExt for mir::Entity {
 }
 
 trait StatementExt {
-    fn lower(&self) -> lir::Statement;
+    fn lower(&self) -> Vec<lir::Statement>;
 }
 
 impl StatementExt for mir::Statement {
-    fn lower(&self) -> spade_lir::Statement {
+    fn lower(&self) -> Vec<lir::Statement> {
         match self {
             spade_mir::Statement::Binding(binding) => todo!(),
             spade_mir::Statement::Register(register) => todo!(),
-            spade_mir::Statement::Constant(value_name, _, constant_value) => todo!(),
+            spade_mir::Statement::Constant(value_name, ty, val) => {
+                vec![lir::Statement::Constant(value_name.lower_fwd(), ty.clone(), val.clone())]
+            },
             spade_mir::Statement::Assert(loc) => todo!(),
             spade_mir::Statement::Set { target, value } => todo!(),
-            spade_mir::Statement::WalTrace { name, val, suffix, ty } => todo!(),
+            spade_mir::Statement::WalTrace {
+                name,
+                val,
+                suffix,
+                ty,
+            } => todo!(),
             spade_mir::Statement::Error => todo!(),
         }
     }
